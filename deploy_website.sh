043d5db703e9442dc93b30a66f7dc07c4f21bf39
Ldq <brian.lecarpentier@edu.itescia.fr>
2021-04-28 12:17:06 +0200

    fix script functionnal

--- deploy_website.sh
+++ deploy_website.sh
@@ -8,45 +8,55 @@ WORDPRESS="https://fr.wordpress.org/latest-fr_FR.tar.gz"
 read -p "Enter a project name : " projectname
 read -s -p "Enter password : " password
 egrep "^$projectname" /etc/passwd >/dev/null
-echo $?
 exist=$?
+echo $exist
 
-if [ $exist ]; then
+if [ $exist -eq 0 ]; then
   echo "projectname exists!"
   exit 1
 else
   arrIN=(${projectname//./ })
 
-  username="${arrIN[1]}"
+  username="${arrIN[0]}"
   sudo useradd -m -G "www-data" -p "$password" "$username"
 
   [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
 
-  mysql -u root -p -e "CREATE DATABASE $username CHARACTER SET UTF8 COLLATE UTF8_BIN;"
-  mysql -u root -p -e "CREATE USER '$username'@'%' IDENTIFIED BY '$password';"
-  mysql -u root -p -e "GRANT ALL PRIVILEGES ON $username.* TO '$username'@'%';"
-  mysql -u root -p -e "FLUSH PRIVILEGES;"
+  mysql -u root "" -e "CREATE DATABASE $username CHARACTER SET UTF8 COLLATE UTF8_BIN;"
+  mysql -u root "" -e "CREATE USER '$username'@'%' IDENTIFIED BY '$password';"
+  mysql -u root "" -e "GRANT ALL PRIVILEGES ON $username.* TO '$username'@'%';"
+  mysql -u root "" -e "FLUSH PRIVILEGES;"
 
-  sudo mkdir "var/www/$username"
+  mkdir "/var/www/$username"
+  read -p "Mkdir : "
+  echo "J'ai mkdir"
 
-  sudo cd "var/www/$username" || return
+  cd "/var/www/$username" || return
+  read -p "cd var/www : "
+  echo "J'ai CD"
 
   # wget to download VERSION file
   $wget "${WORDPRESS}"
 
+  read -p "je vais dezipper : "
+
   $tar xvf "latest-fr_FR.tar.gz"
 
-  sudo mv -r "wordpress/*" "var/www/$username/"
+  sudo mv "/var/www/$username/wordpress/"* "/var/www/$username"
+
+  read -p "mv wordpress : "
 
   sudo rm -r "wordpress"
 
+  read -p "je remove unused dir : "
+
   sudo cp /etc/apache2/sites-available/template /etc/apache2/sites-available/$projectname
 
-  sudo sed -i 's/template/'$username'/g' /etc/apache2/sites-available/$projectname
+  sudo sed -i 's/template/'$username'/g' /etc/apache2/sites-available/$projectname".conf"
 
-  sudo sed -i '1s/^/192.168.1.61       '$projectname'\n/' /etc/hosts
+  sudo sed -i '1s/^/192.168.1.61  '$projectname'\n/' /etc/hosts
 
-  sudo a2ensite $projectname
+  sudo a2ensite $projectname".conf"
   sudo service apache2 reload
 
   echo "Done, please browse to http://$projectname to check!"
