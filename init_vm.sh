567736289bf63c9f06e696538ea0f5758572ac54
Ldq <brian.lecarpentier@edu.itescia.fr>
2021-04-29 16:47:27 +0200

    Update init_vm.sh

--- init_vm.sh
+++ init_vm.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+########################################################################
+# This script is used to initialiaze a VM                              #
+########################################################################
+
 sudo apt -y update
 
 sudo apt -y install net-tools
 sudo apt-get -y install apache2
+sudo apt-get -y install openssh-server
 sudo apt-get -y install mysql-server mysql-client
 sudo apt-get -y install php7.4 php7.4-mysql libapache2-mod-php7.4
 
